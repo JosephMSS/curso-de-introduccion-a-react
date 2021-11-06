88e1335900094dd01a20f9d9ca5faa2b7bc9ca06
Joseph Morales Silva <josephzx1@hotmail.com>
2021-11-06 14:34:43 -0600

    Add dropshadow to seach component

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-function TodoSearch({ id, label, placeholder }) {
+function TodoSearch({ id, placeholder }) {
   return (
-    <div className="flex flex-col">
-      <label className=" ml-1 pb-1 font-poppins " htmlFor={id}>{label}</label>
-      <div className=" rounded-lg shadow-lg flex items-center align-middle border">
-        <FaSearch className="bg-white  text-primary pl-2 h-5 w-8 flex-shrink" />
-        <input
-          id={id}
-          className="ml-2 py-3 pl-2 h-full w-full rounded-lg placeholder-primary placeholder-opacity-70 focus:outline-none focus:ring focus:ring-inset focus:border-quaternary "
-          type="text"
-          placeholder={placeholder}
-        />
+    <>
+      <div className="flex flex-col relative ">
+        <div
+          className="absolute inset-1
+        bg-gradient-to-r from-primary to-quaternary 
+        rounded-lg blur opacity-10 -ml-2 -mt-2
+        
+        "
+        ></div>
+        <div className="relative bg-white rounded-lg flex items-center align-middle border">
+          <FaSearch className="  text-primary pl-2 h-5 w-8 flex-shrink" />
+          <input
+            id={id}
+            className="ml-2 py-3 pl-2 h-full w-full rounded-lg placeholder-primary placeholder-opacity-70 focus:outline-none focus:ring focus:ring-inset focus:border-quaternary "
+            type="text"
+            placeholder={placeholder}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
