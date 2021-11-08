67bd5fe64a981b1496002e0edb89da45df43f105
Joseph Morales Silva <josephzx1@hotmail.com>
2021-11-07 21:24:19 -0600

    Add icons to Todo Item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,23 @@
-import { Fragment } from "react";
-
+import React from "react";
+import { BiCheckCircle } from "react-icons/bi";
+import { AiOutlineEdit } from "react-icons/ai";
+import { AiOutlineDelete } from "react-icons/ai";
 function TodoItem(props) {
   return (
-    <Fragment>
-      <li>
-        <span>C</span>
-        <p>{props.text}</p>
-        <span>X</span>
+    <>
+      <li className="grid  grid-cols-8 my-3 ">
+        <span className="my-auto mx-1 col-span-1">
+          <BiCheckCircle className="text-xl" />
+        </span>
+        <p className="col-span-5 truncate mx-1">{props.text}</p>
+        <span className="mx-1">
+          <AiOutlineEdit className="text-xl" />
+        </span>
+        <span className="mx-1">
+          <AiOutlineDelete className="text-xl" />
+        </span>
       </li>
-    </Fragment>
+    </>
   );
 }
 
